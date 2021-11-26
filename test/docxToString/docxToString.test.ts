2ffb8c2b908640551b2342c14bb4a2cd69abf589
Said Torres <sesaid17@hotmail.com>
2021-11-26 12:43:06 -0600

    Made test functions asyncronous

--- test/docxToString/docxToString.test.ts
+++ test/docxToString/docxToString.test.ts
@@ -3,24 +3,21 @@ import fs from 'fs'
 
 jest.setTimeout(20000)
 
-test('Parse simple document.', () => {
-  return docxToString('test/docxToString/simpleDocument/docx.docx').then((output: string) => {
-    const expectedOutput = fs.readFileSync('test/docxToString/simpleDocument/expectedOutput.txt', {encoding: 'utf8'})
-    expect(output).toBe(expectedOutput)
-  });
+test('Parse simple document.', async () => {
+  const output = await docxToString('test/docxToString/simpleDocument/docx.docx');
+  const expectedOutput = fs.readFileSync('test/docxToString/simpleDocument/expectedOutput.txt', { encoding: 'utf8' });
+  expect(output).toBe(expectedOutput);
 });
 
-test('Parse document with a image.', () => {
-  return docxToString('test/docxToString/documentWithImg/eldestierrodelrayo.docx').then((output: string) => {
-    const expectedOutput = fs.readFileSync('test/docxToString/documentWithImg/expectedOutput.txt', {encoding: 'utf8'})
-    expect(output).toBe(expectedOutput)
-  });
+test('Parse document with a image.', async () => {
+  const output = await docxToString('test/docxToString/documentWithImg/eldestierrodelrayo.docx');
+  const expectedOutput = fs.readFileSync('test/docxToString/documentWithImg/expectedOutput.txt', { encoding: 'utf8' });
+  expect(output).toBe(expectedOutput);
 });
 
-test('Parse a large document.', () => {
-  return docxToString('test/docxToString/largeDocument/template.docx').then((output: string) => {
-    const expectedOutput = fs.readFileSync('test/docxToString/largeDocument/expectedOutput.txt', {encoding: 'utf8'})
-    fs.writeFileSync('test/docxToString/largeDocument/output.txt', output)
-    expect(output).toBe(expectedOutput)
-  });
+test('Parse a large document.', async () => {
+  const output = await docxToString('test/docxToString/largeDocument/template.docx');
+  const expectedOutput = fs.readFileSync('test/docxToString/largeDocument/expectedOutput.txt', { encoding: 'utf8' });
+  fs.writeFileSync('test/docxToString/largeDocument/output.txt', output);
+  expect(output).toBe(expectedOutput);
 });
