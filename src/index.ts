884d03f81ae0f1d5dd7cb95070d5fa308c9f1c1e
Said Torres <sesaid17@hotmail.com>
2021-11-26 16:08:35 -0600

    Added data.json to the html differ

--- src/index.ts
+++ src/index.ts
@@ -16,29 +16,35 @@ const html = readFileSync('./src/diffTemplate.html', 'utf8')
 compareTwoDocx("src/out.docx", "src/template.docx").then(diff => {
   const dom = new JSDOM(html)
   // count if add and remove has the same amount
-  const removed = diff.filter(item => item.removed).length
-  const added = diff.filter(item => item.added).length
-
-  console.log("Removed: " + removed, ",Added: " + added)
-  diff.forEach((part) => {
+  const elementOne = dom.window.document.getElementById("output")
+  const elementTwo = dom.window.document.getElementById("template")
+  if (!elementOne || !elementTwo) { return }
+  console.log('init forEach')
+  diff.forEach((change) => {
     // green for additions, red for deletions, grey for common parts
-    const color = part.added ? 'green' :
-      part.removed ? 'red' : 'white'
-
-    const elementOne = dom.window.document.getElementById("output")
-    const elementTwo = dom.window.document.getElementById("template")
-
-    if (part.added) {
-      if (elementOne) elementOne.innerHTML += `<span style="color: ${color}">${part.value}</span>`
-    } else if (part.removed) {
-      if (elementTwo) elementTwo.innerHTML += `<span style="color: ${color}">${part.value}</span>`
+    if (change.added) {
+      elementOne.innerHTML += `<span style="color:green">${change.value}</span>`
+    } else if (change.removed) {
+      elementTwo.innerHTML += `<span style="color:red">${change.value}</span>`
     } else {
-      if (elementOne) elementOne.innerHTML += `<span>${part.value}</span>`
-      if (elementTwo) elementTwo.innerHTML += `<span>${part.value}</span>`
+      elementOne.innerHTML += `<span>${change.value}</span>`
+      elementTwo.innerHTML += `<span>${change.value}</span>`
     }
-
-    // space in html: &nbsp;
   })
+  console.log('finished')
+
+  const dataShowerElement = dom.window.document.getElementById('data-shower')
+  const data: JSON = JSON.parse(readFileSync('src/data.json', 'utf8'))
+  function printObject(object: JSON, path: string) {
+    if (object instanceof Object) {
+      Object.entries(object).forEach(([key, value]) => printObject(value, `${path}.${key}`))
+    } else {
+      if (dataShowerElement) dataShowerElement.innerHTML += (path + ": " + object + `\r\n`)
+      // console.log(path, object)
+    }
+  }
+  printObject(data, 'data')
+  // add data to dom in 'data-shower' element
 
   const htmlDiff = dom.serialize()
   writeFileSync('src/diff.html', htmlDiff)
